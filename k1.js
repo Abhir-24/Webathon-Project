f0b51f101b1b18e355f43f00e53ea41a6cb384c5
Abhir Raj Shrivastava <abhir.raj2410@gmail.com>
2020-02-03 21:57:46 +0530

    Webathon

---
+++ k1.js
@@ -0,0 +1,7 @@
+
+var a,b,c;
+	a= parseInt(prompt("enter first no."));
+    b= parseInt(prompt("enter second no."));
+    c = a+b;
+    document.write("Addition of two numbers is "+c);
+    console.log(c);
---
+++ k4.js
@@ -0,0 +1,7 @@
+
+var a,b,c;
+	a= parseInt(prompt("enter first no."));
+    b= parseInt(prompt("enter second no."));
+    c = a/b;
+    document.write("Division of two numbers is "+c);
+    console.log(c);//will print the value of c in console
